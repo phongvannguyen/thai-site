7938034a5d2e12bf2e2ed95b4bca7ecd4f253443
phongnguyen2030 <phongnguyen2030@github.com>
2020-03-08 17:44:08 -0400

    Menu is good

--- src/app/body/body.component.ts
+++ src/app/body/body.component.ts
@@ -7,37 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BodyComponent implements OnInit {
 
-  public isAppetizer = true;
-  public isSalad = false;
-  public isSoup = false;
-  public isDesertsDrinks = false;
-  public menus = ['Appetizer', 'Salad', 'Soup', 'Desserts & Drinks'];
+  /* Common Menus of Lunch & Dinner*/
+  public menuClicked = 'Appetizer';
+  public commonMenus = ['Appetizer', 'Salad', 'Soup', 'Desserts & Drinks'];
   constructor() { }
 
   ngOnInit(): void {
   }
 
-  public showMenu(menu: string): void {
-    if (menu === 'Appetizer') {
-      this.isAppetizer = true;
-      this.isSalad = false;
-      this.isSoup = false;
-      this.isDesertsDrinks = false;
-    } else if (menu === 'Salad') {
-      this.isSalad = true;
-      this.isAppetizer = false;
-      this.isSoup = false;
-      this.isDesertsDrinks = false;
-    } else if (menu === 'Soup') {
-      this.isSoup = true;
-      this.isAppetizer = false;
-      this.isSalad = false;
-      this.isDesertsDrinks = false;
-    } else if (menu === 'Desserts & Drinks') {
-      this.isDesertsDrinks = true;
-      this.isAppetizer = false;
-      this.isSalad = false;
-      this.isSoup = false;
-    }
+  /* Common Menus of Lunch & Dinner*/
+  public setMenu(menu: string): void {
+    this.menuClicked = menu;
+  }
+
+  public showMenu(menu: string): boolean {
+    return this.menuClicked === menu;
   }
 }
